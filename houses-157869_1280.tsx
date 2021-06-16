ebc1ccd39167300a0afafbad7742cf7bc9b257b9
ResiPu <resy-@gmx.de>
2021-06-16 11:01:44 +0200

    bäume, haus

---
+++ houses-157869_1280.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="houses-157869_1280" tilewidth="32" tileheight="32" tilecount="1520" columns="38">
+ <image source="houses-157869_1280.png" width="1223" height="1280"/>
+</tileset>
