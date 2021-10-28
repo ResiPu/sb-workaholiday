e922cda1ea89588c6ae2f29d3125f56119f0f39b
ResiPu <resy-@gmx.de>
2021-10-28 11:24:55 +0200

    dd

---
+++ neu_pool.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pool" tilewidth="32" tileheight="32" tilecount="420" columns="20">
+ <image source="flowers-painted.png" width="668" height="674"/>
+</tileset>
---
+++ pflanzen.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pflanzen" tilewidth="41" tileheight="64" tilecount="42" columns="7">
+ <image source="farming/plants.png" width="288" height="384"/>
+</tileset>
